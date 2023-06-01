7d55a58f35608139d30adf58db99716d9ed095f2
Asahel Enock Kigen <kirwaasahel@gmail.com>
2023-06-01 11:09:49 +0300

    [UPDATE]: Cors origin update

--- src/main/java/com/example/Faida/config/CorsConfig.java
+++ src/main/java/com/example/Faida/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://faida-dashboard.vercel.app")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
